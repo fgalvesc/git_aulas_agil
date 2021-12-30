835d4da324739d47456c4948af200349f20471a6
Felipe Costa <alvesfgc@gmail.com>
2021-12-30 20:14:01 +0000

    feat: Implementing something in test branch

--- messages.py
+++ messages.py
@@ -3,4 +3,6 @@ display_messages = [
     'Fique Tranquilo, tudo vai acabar bem!'
     'Olá Mundo, estou aqui!'
     'Acredite nos seus caminhos! Sempre!'
+    'Sempre apague as luzes antes de sair'
+    'Estamos aqui aprendendo sobre Branches'
 ]
